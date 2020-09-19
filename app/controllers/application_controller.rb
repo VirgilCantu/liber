49bf52e7a55db6091bc3e178a0f10fa35ee5ba7e
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-19 17:36:16 -0500

    added session secret in configure block, application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-  end
-
-  get "/" do
-    erb :welcome
+    set :session_secret, "liber_secret"
   end
 
 end
