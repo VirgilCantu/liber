b145821b2fd96d741bc90be570d407af91de1c07
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-20 20:09:41 -0500

    added post /books route to create new book objects in users collection

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
     
-    get "/new" do
+    get "/books/new" do
         erb :"/books/new"
     end
 
-    post "/new" do
-    
+    post "/books" do
+        @book = Book.create(params)
+
+        redirect to "/books/#{@book.id}"
     end
+
 end
