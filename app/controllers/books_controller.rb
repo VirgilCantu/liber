0034e9e9abfced04b9432b32ec905239915a9807
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-20 20:38:12 -0500

    finished post /books route, created get books/:id route to display a particular book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,24 @@
 class BooksController < ApplicationController
     
+    get '/books' do
+        @books = Book.all
+        erb :'/books/index'
+      end
+        
     get "/books/new" do
         erb :"/books/new"
     end
 
     post "/books" do
         @book = Book.create(params)
-
+        @book.owner = current_user
         redirect to "/books/#{@book.id}"
     end
+
+    get '/books/:id' do
+        @book = Book.find(params[:id])
+        erb :'/books/show'
+      end
+    
 
 end
